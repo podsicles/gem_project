73ee0ef73e147188925987e94823c75097d23cc7
podsicles <jessicaflores.business@gmail.com>
2023-08-03 13:33:18 +0800

    [Feature 2] create admin and client constraint

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'home#index'
     get '/', to: 'admin/home#index', as: :admin_root
     get '/', to: 'client/home#index', as: :client_root
+  constraints(ClientDomainConstraint.new) do
+  end
+  constraints(AdminDomainConstraint.new) do
+  end
 end
