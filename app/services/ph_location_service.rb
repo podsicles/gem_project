69674472257de10865b65bfcb032327a5a269ea2
podsicles <jessicaflores.business@gmail.com>
2023-08-18 18:36:04 +0800

    [Feature 5] add method fetch provinces

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -14,4 +14,16 @@ class PhLocationService
       address_region.save
     end
   end
+
+  def fetch_provinces
+    request = RestClient.get("#{url}/provinces")
+    data = JSON.parse(request.body)
+    data.each do |province|
+      region = Address::Region.find_by_code(province['regionCode'])
+      address_province = Address::Province.find_or_initialize_by(code: province['code'])
+      address_province.name = province['name']
+      address_province.region = region
+      address_province.save
+    end
+  end
 end
