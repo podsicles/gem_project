22dac148861f52912a512febe1c30f7ae6ade0a7
podsicles <jessicaflores.business@gmail.com>
2023-08-18 17:19:55 +0800

    [Feature 5] create model Address::Region

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_053448) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_091519) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
